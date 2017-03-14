225bd2c46ca4410e9232f9f821f4aa81022e02a3
danil <danil@LAPTOP-B6VH6P5A>
2017-03-14 02:47:05 -0300

    Tags para autorizar ciertos roles.

--- Web/Controllers/CommonController.cs
+++ Web/Controllers/CommonController.cs
@@ -1,21 +1,25 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class CommonController : Controller
     {
+        ApplicationDbContext context = new ApplicationDbContext();
+
         /// <summary>
         /// Método útil para setear un par de tempData comunmente usado para enviar mensajes a la vista luego de un redirect.
         /// </summary>
         /// <param name="msg">Mensaje</param>
         /// <param name="type">tipo, usar:  success, error</param>
-        /// 
-
-      
+        ///     
         public void SetTempData(string msg, string type = "success")
         {
             TempData["message"] = msg;
@@ -32,6 +36,35 @@ namespace Web.Controllers
 #else
             SetTempData(errorMessage,"error");
 #endif
+        }
+
+        public bool isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool isMozoUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Mozo")
+                {
+                    return true;
+                }                    
+            }
+            return false;
         }
     }
 }
