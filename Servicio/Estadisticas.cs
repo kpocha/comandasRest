7e248564c81c6a4eb7966da0bea4c874d8501a4d
Danilo Pérez Ortega <danilo_p_o@hotmail.com>
2017-03-20 17:19:28 -0300

    helper object to json - Estructura para estadisticas

---
+++ Servicio/Estadisticas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Entidad;
+using Web.Entidad.Models;
+
+namespace Servicio
+{
+    public class Estadisticas
+    {
+        IUnitOfWork unitOfWork;
+
+        public Estadisticas() : this(new UnitOfWork()) { }
+
+        public Estadisticas(IUnitOfWork uow)
+        {
+            unitOfWork = uow;
+        }
+        /// <summary>
+        /// Encuentra las comandas que se hicieron en el dia de la fecha.
+        /// </summary>
+        /// <returns>Lista de comandas</returns>
+        public List<Comandas> ListComandaDay()
+        {
+            DateTime today = new DateTime();
+            var comandasToday = unitOfWork.ComandasRepository.Filter(a => a.fecha.Day == today.Day);
+
+            return comandasToday.ToList();
+        }
+        /// <summary>
+        /// Lista las comandas que se hicieron en corriente mes.
+        /// </summary>
+        /// <returns>Lista de comandas</returns>
+        public List<Comandas> ListComandasMonth()
+        {
+            DateTime today = new DateTime();
+            var comandasToday = unitOfWork.ComandasRepository.Filter(a => a.fecha.Month == today.Month);
+            
+            return comandasToday.ToList();
+        }
+        
+    }
+}
---
+++ Web/Controllers/EstadisticasController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Entidad;
+using Web.Entidad.Models;
+using Web.Helpers;
+
+namespace Web.Controllers
+{
+    [AuthLog(Roles = "Admin")]
+    public class EstadisticasController : CommonController
+    {
+        IUnitOfWork unitOfWork;
+
+        public EstadisticasController() : this(new UnitOfWork()) { }
+
+        public EstadisticasController(IUnitOfWork uow)
+        {
+            unitOfWork = uow;
+        }
+        
+        public String ListComandaDay()
+        {
+            DateTime today = new DateTime();
+            List<Categorias> comandasToday = unitOfWork.CategoriasRepository.All().ToList();
+            //example how to use helper json
+            JsonHelper helper = new JsonHelper();
+            String jsonResult = helper.ConvertObjectToJSon(comandasToday);
+            return jsonResult;
+        }
+
+        
+    }
+}
