8e5f22a22ab17e3ed4a65d6776d77ccd506b39f9
Danilo Pérez Ortega <danilo_p_o@hotmail.com>
2017-03-21 02:59:38 -0300

    mejoras vista dispositivo movil - Utils fecha cambiada por tiempo restante, se extendio la clase comandas

---
+++ Entidad/ModelsExtension/Comandas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Entidad.Models
+{
+    public partial class Comandas
+    {
+        public string timeAgo { get; set; }
+        
+    }
+}
---
+++ Servicio/Utils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio
+{
+    public class Utils
+    {
+        public string TimeAgo(DateTime dt)
+        {
+            TimeSpan span = DateTime.Now - dt;
+            if (span.Days > 365)
+            {
+                int years = (span.Days / 365);
+                if (span.Days % 365 != 0)
+                    years += 1;
+                return String.Format("hace {0} {1}",
+                years, years == 1 ? "año" : "años");
+            }
+            if (span.Days > 30)
+            {
+                int months = (span.Days / 30);
+                if (span.Days % 31 != 0)
+                    months += 1;
+                return String.Format("hace {0} {1}",
+                months, months == 1 ? "mes" : "meses");
+            }
+            if (span.Days > 0)
+                return String.Format("hace {0} {1}",
+                span.Days, span.Days == 1 ? "día" : "días");
+            if (span.Hours > 0)
+                return String.Format("hace {0} {1}",
+                span.Hours, span.Hours == 1 ? "hora" : "horas");
+            if (span.Minutes > 0)
+                return String.Format("hace {0} {1}",
+                span.Minutes, span.Minutes == 1 ? "minuto" : "minutos");
+            if (span.Seconds > 5)
+                return String.Format("hace {0} segundos", span.Seconds);
+            if (span.Seconds <= 5)
+                return "justo ahora";
+            return string.Empty;
+        }
+
+    }
+
+}
